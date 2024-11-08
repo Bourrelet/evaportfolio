7eabbc50a5175a86e702198bbf95c74d7e9f8e08
Eva <eva.boulanger4@gmail.com>
2024-11-08 23:25:17 +0100

    Portefolio fonctionnel

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react'; // Ajoute `useState` à l'import
+import Project from './Project'; // Assure-toi d'importer `Project` correctement
 
 const Card = ({id, title, cover, description, samples, link}) => {
 
@@ -10,7 +11,7 @@ const Card = ({id, title, cover, description, samples, link}) => {
 
   return (
     <div className='card'>
-      {viewProject ? <Project samples={samples} link={link} backspace={toggleView}/> : '' }      
+      {viewProject ? <Project samples={samples} link={link} closeModale={toggleView}/> : '' }      
       <div className='card__content'>
         <h3 className='card__content__title'>{title}</h3>
         <p className='card__content_txt'>{description}</p>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react'; // Ajoute `useState` à l'import
 
-function Project({id}) {
+
+
+
+function Project({samples, link, closeModale}) {
+
+  
+  const prevBtn = "<=";
+  const nextBtn = "=>";
+
+  const [activeIndex , setActiveIndex] = useState(0);
+
+    const prevImg = () => {
+        setActiveIndex((prevState) => (prevState === 0 ? samples.length - 1 : prevState - 1));
+    };
+
+    const nextImg = () => {
+        setActiveIndex((prevState) => (prevState === samples.length - 1 ? 0 : prevState + 1));
+    };
+
 
   return (
-    <div>
+    <div className='project'>
+      <button className='project__closeBtn' onClick={closeModale}> X </button>
+      <div className='project__gallery'>
+        <img className='gallerie__img' src={samples[activeIndex].image} alt={`${activeIndex} + 1`}/>
+        {samples.length > 1 && (
+        <div className='gallerie__nav'>
+          <div className='gallerie__nav__btn' onClick={prevImg}>{prevBtn}</div>
+          <div className='gallerie__nav__btn' onClick={nextImg}>{nextBtn}</div>
+          <p className='gallerie__index'>{`${activeIndex + 1} / ${samples.length}`}</p>
+        </div>)}
+      </div>
+      <div className='project__content'>
+        <p className='project__content__txt' >{samples[activeIndex].comment}</p>
+      </div>
+      <div className='project__link'>{link}</div>
       
     </div>
   );
