e012f13594460cadd49a81c0a72c9f877e03bcf9
Bourrelet <eva.boulanger4@gmail.com>
2024-11-08 15:21:52 +0100

    React router initiated

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.scss';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Presentation from './pages/Presentation';
+import Realisations from './pages/Realisations';
+import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        {/* Route parent avec Layout */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Presentation />} />
+          <Route path="realisations" element={<Realisations />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Presentation.js
@@ -0,0 +1,13 @@
+// src/pages/Presentation.js
+import React from 'react';
+
+function Presentation() {
+  return (
+    <div>
+      <h1>Présentation</h1>
+      <p>Bienvenue ! Je suis Eva, développeuse passionnée par le web.</p>
+    </div>
+  );
+}
+
+export default Presentation;
