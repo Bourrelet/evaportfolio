b8412129a3cc4bb8c8234d082002034ce1485227
Eva <eva.boulanger4@gmail.com>
2024-11-11 20:29:31 +0100

    Wip - Projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -27,28 +27,33 @@ function Project({samples, link, closeModale}) {
     };
 
 
+    const [bannerView , setBannerView] = useState(true);
+    const toggleBanner = () => {
+      setBannerView(!bannerView);
+    }
+    const bannerAnimation = `${bannerView ? 'bannerYes' : 'bannerNo'}`;
+
+
+
+
+
   return (
     <div className='project'>
       
-      <div className='project__gallery'>
+      <div className='project__gallery' onMouseEnter={toggleBanner} onMouseLeave={toggleBanner}>
         <img className='project__gallery__img' src={samples[activeIndex].image} alt={`${activeIndex + 1}`}/>
         {samples.length > 1 && (
         <div className='project__gallery__nav'>
-
-          <div className='project__gallery__nav__btn'>
-
-            <div className='project__gallery__nav__btn-prev' onClick={prevImg}>{prevBtn}</div>
-            <div className='project__gallery__nav__btn-next' onClick={nextImg}>{nextBtn}</div>
-
-          </div>
-
-          <p className='project__gallery__nav__index'>{`${activeIndex + 1} / ${samples.length}`}</p>
-
-        </div>)}
-        
+          <p className='project__gallery__nav__index'>{`${activeIndex + 1} / ${samples.length}`}</p>        
+        </div>)}        
       </div>
 
-      <div className='project__content'>
+      <div className={`project__content ${bannerAnimation} `}>
+      {samples.length > 1 && (
+        <div className='project__content__nav'>
+            <div className='project__content__nav__btn-prev' onClick={prevImg}>{prevBtn}</div>
+            <div className='project__content__nav__btn-next' onClick={nextImg}>{nextBtn}</div>
+        </div>)}
         <p className='project__content__txt' >{samples[activeIndex].comment}</p>
         <div className='project__content__btn'> 
           <button className='project__content__btn__close' onClick={closeModale}> Close Project </button>
