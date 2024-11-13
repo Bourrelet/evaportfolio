eed0dea7ad02104c1d848026f5dcbe2213bfde0f
Eva <eva.boulanger4@gmail.com>
2024-11-14 00:25:50 +0100

    CSS Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 // src/components/Footer.js
 import React from 'react';
+import '../styles/Footer.scss'
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; 2024 Eva Portfolio</p>
+    <footer className='footer'>
+      
+      <div className='footer__link'>
+        <a href="mailto:eva.boulanger90@gmail.com">Send me an email.</a> 
+        <a href="https://www.linkedin.com/in/eva-boulanger-ba734227a" target="_blank" rel="noopener noreferrer">Follow me on LinkedIn</a>
+        <a href="https://github.com/Bourrelet" target="_blank" rel="noopener noreferrer">Check out my GitHub.</a>
+      </div>
+
+        
+      <p className='footer__copyright'>
+        &copy; 2024 Eva Boulanger Portfolio
+      </p>
     </footer>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,21 +7,22 @@ function About() {
 
       <div className='aboutMe__content'>
 
-        <div className='aboutMe__content__title'>
-          <h2>Splendid, perfectly.</h2>
-        </div>
-        <div className='aboutMe__content__description'>
-        <p>I started my journey as a Developer on FreeCodeCamp, I got two certifications, one in responsive web design and another in javascript algorithms. 
-             Then I followed the OpenClassrooms Web Developer program to obtain an actual qualification, and improve my skills by completing real projects. <br/>
-             I’m eager to dive deeper into tech, aiming to gain the skills to achieve my ambitions of wealth and glory. <br/>
-            I enjoy projects that challenge me and let me grow, valuing clarity, thoroughness, and technical depth in all I do.
-            </p>
-          
-        </div>
+        <div className='aboutMe__content__banner'>
 
-        <button className="aboutMe__content__resume" onClick={() => window.open("https://drive.google.com/file/d/1oIPyKeSr0YKJaHK1Clt3bS2VRK5plL0y/view?usp=drive_link", "_blank", "noopener,noreferrer")}>
-        Resume
-        </button>
+          <h2>Splendid, perfectly.</h2>
+          <button className="aboutMe__content__banner__resume" onClick={() => window.open("https://drive.google.com/file/d/1oIPyKeSr0YKJaHK1Clt3bS2VRK5plL0y/view?usp=drive_link", "_blank", "noopener,noreferrer")}>
+            Resume
+          </button>
+
+        </div>
+        
+        <div className='aboutMe__content__description'>
+          <p>I started my journey as a Developer on FreeCodeCamp, I got two certifications, one in responsive web design and another in javascript algorithms. 
+              Then I followed the OpenClassrooms Web Developer program to obtain an actual qualification, and improve my skills by completing real projects. <br/>
+              I’m eager to dive deeper into tech, aiming to gain the skills to achieve my ambitions of wealth and glory. <br/>
+              I enjoy projects that challenge me and let me grow, valuing clarity, thoroughness, and technical depth in all I do.
+          </p>          
+        </div>
 
       </div>
 
